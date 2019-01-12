1885247374d466bf42af330781db491df41be480
cibergarri@hotmail.com <javier.garridoiglesias@cognizant.com>
2019-01-12 22:29:11 +0100

    fix rout

--- react_authorquizz/service-worker.js
+++ react_authorquizz/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6adc975bbc201fb551d121c306cc270.js"
+  "./precache-manifest.069918b8e65ee55ce18fc3f975f65295.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
