52fe9f8991f1b642f80121af733eb9ac4962e5a2
cibergarri@hotmail.com <javier.garridoiglesias@cognizant.com>
2019-01-13 22:09:52 +0100

    Updates

---
+++ react_authorquizz/precache-manifest.a8b4220d82ecaa45aab16b0e4a78241a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e9d07d0a688c7d4df3c",
+    "url": "/react_authorquizz/static/css/main.dc12abec.chunk.css"
+  },
+  {
+    "revision": "9e9d07d0a688c7d4df3c",
+    "url": "/react_authorquizz/static/js/main.9e9d07d0.chunk.js"
+  },
+  {
+    "revision": "4cf2a3c2febd79979c5e",
+    "url": "/react_authorquizz/static/js/1.4cf2a3c2.chunk.js"
+  },
+  {
+    "revision": "6b4de26d294e29baf4e0",
+    "url": "/react_authorquizz/static/js/runtime~main.6b4de26d.js"
+  },
+  {
+    "revision": "a8fa7d1e82d651afed77da24ac592490",
+    "url": "/react_authorquizz/index.html"
+  }
+];
