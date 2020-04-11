81451c46c13bfd803ddfec8df2f573d8b9e254e8
Garri <cibergarri@hotmail.com>
2020-04-11 17:42:46 +0200

    Updates

---
+++ precache-manifest.05bf5cb08bf86932129a12e1c0ebd9cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7276722759b4f34d5d82610662c3eac",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "af5b4e64581c1c802580",
+    "url": "/home/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c96304b3671b9a9e7a99",
+    "url": "/home/static/js/2.9233cb89.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/home/static/js/2.9233cb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5b4e64581c1c802580",
+    "url": "/home/static/js/main.ea06c5ce.chunk.js"
+  },
+  {
+    "revision": "73881734ccfb416536bd",
+    "url": "/home/static/js/runtime-main.fd09b83d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cibergarri.github.io/home/precache-manifest.0df58bf5d3679c14ca0d56cdb5bfb080.js"
+  "/home/precache-manifest.05bf5cb08bf86932129a12e1c0ebd9cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cibergarri.github.io/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
