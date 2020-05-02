c549793a3db27c465cf9d7ca044086f578f1dd1e
Garri <cibergarri@hotmail.com>
2020-05-02 19:06:27 +0200

    chore(ci): use travis
    add travis config
    add automatic-release

---
+++ release.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable no-restricted-syntax */
+module.exports = {
+  branch: 'master',
+  debug: true,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    [
+      '@semantic-release/npm', {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'pack/*.tgz',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        publishCmd: './publish.sh ${nextRelease.version} ${options.branch} ${commits.length} ${Date.now()}',
+      },
+    ],
+  ],
+  preset: 'angular',
+};
