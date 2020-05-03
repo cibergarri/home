b1da236a00ad06f5c9b1296c51a87677011e49a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-03 21:34:25 +0000

    Deploy cibergarri/home to github.com/cibergarri/home.git:gh-pages

---
+++ precache-manifest.029738f1170721b50e8de8b7f86b6dd1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5345c51b304d28f91ae4c9a179dcb27a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a74ec0ab2d5c991de613",
+    "url": "/home/static/css/main.1ea0a779.chunk.css"
+  },
+  {
+    "revision": "b6146e26b183ee5f7660",
+    "url": "/home/static/js/2.c0811577.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/home/static/js/2.c0811577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8894707270898db87465",
+    "url": "/home/static/js/3.003ac3dc.chunk.js"
+  },
+  {
+    "revision": "6d1d334f5d9499ac483f",
+    "url": "/home/static/js/4.b9c219c0.chunk.js"
+  },
+  {
+    "revision": "a74ec0ab2d5c991de613",
+    "url": "/home/static/js/main.ce999fc7.chunk.js"
+  },
+  {
+    "revision": "b8c729e9e08cc96ebff6",
+    "url": "/home/static/js/runtime-main.14ac9575.js"
+  },
+  {
+    "revision": "7ffa3709a9783339890da178a01276a7",
+    "url": "/home/static/media/bg-night-orig.7ffa3709.png"
+  },
+  {
+    "revision": "40511c7be17581f4300f1a09b9c411af",
+    "url": "/home/static/media/bg-night.40511c7b.png"
+  },
+  {
+    "revision": "12b8d264ff5f551c3af2f12ef6011c81",
+    "url": "/home/static/media/bg-normal-orig.12b8d264.png"
+  },
+  {
+    "revision": "a1fea794955bbee0a80d23b3140861a6",
+    "url": "/home/static/media/bg-normal.a1fea794.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.b26b1081ef086ecca0df9648abd28b33.js"
+  "/home/precache-manifest.029738f1170721b50e8de8b7f86b6dd1.js"
 );
 
 self.addEventListener('message', (event) => {
